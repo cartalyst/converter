be3535b074fbb9965d500e1570eca323c9ad774f
Bruno Gaspar <brunofgaspar1@gmail.com>
2014-05-09 20:23:56 +0100

    update copyright headers
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- src/Converter.php
+++ src/Converter.php
@@ -4,16 +4,15 @@
  *
  * NOTICE OF LICENSE
  *
- * Licensed under the 3-clause BSD License.
+ * Licensed under the Cartalyst PSL License.
  *
- * This source file is subject to the 3-clause BSD License that is
- * bundled with this package in the LICENSE file.  It is also available at
- * the following URL: http://www.opensource.org/licenses/BSD-3-Clause
+ * This source file is subject to the Cartalyst PSL License that is
+ * bundled with this package in the license.txt file.
  *
  * @package    Converter
  * @version    1.0.0
  * @author     Cartalyst LLC
- * @license    BSD License (3-clause)
+ * @license    Cartalyst PSL
  * @copyright  (c) 2011-2014, Cartalyst LLC
  * @link       http://cartalyst.com
  */
